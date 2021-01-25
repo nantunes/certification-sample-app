38be8e539bf709be30b761ed5ca5487ae0bbbe38
Nelson Antunes <nelson.antunes@hitachivantara.com>
2021-01-25 15:23:06 +0000

    Install react-router-dom and setup default route
    npm i react-router-dom --save

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { HvProvider, HvFooter } from "@hv/uikit-react-core";
 
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
 import useStyles from "./styles";
 
 import Header from "./components/Header/Header";
@@ -12,15 +14,23 @@ function App() {
 
   return (
     <HvProvider>
-      <Header />
+      <BrowserRouter>
+        <Header />
 
-      <main className={classes.main}>
-        <article className={classes.article}>
-          <Landing />
-        </article>
-      </main>
+        <main className={classes.main}>
+          <article className={classes.article}>
+            <Switch>
+              <Route exact path="/">
+                <Landing />
+              </Route>
 
-      <HvFooter />
+              <Redirect to="/" />
+            </Switch>
+          </article>
+        </main>
+
+        <HvFooter />
+      </BrowserRouter>
     </HvProvider>
   );
 }
